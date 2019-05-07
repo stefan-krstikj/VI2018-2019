1ba6bd49e11f5c6887d925a9c9b434438bc0428e
strayckler <st.krstic@gmail.com>
2019-05-07 11:34:21 +0200

    Finished graf.py

---
+++ Laboratory Exercises/finite_graph_search.py
@@ -0,0 +1,120 @@
+import math
+import random
+
+def distance(a, b):
+    """The distance between two (x, y) points."""
+    return math.hypot((a[0] - b[0]), (a[1] - b [1]))
+
+
+class Graph:
+    """A graph connects nodes (verticies) by edges (links).  Each edge can also
+    have a length associated with it.  The constructor call is something like:
+        g = Graph({'A': {'B': 1, 'C': 2})
+    this makes a graph with 3 nodes, A, B, and C, with an edge of length 1 from
+    A to B,  and an edge of length 2 from A to C.  You can also do:
+        g = Graph({'A': {'B': 1, 'C': 2}, directed=False)
+    This makes an undirected graph, so inverse links are also added. The graph
+    stays undirected; if you add more links with g.connect('B', 'C', 3), then
+    inverse link is also added.  You can use g.nodes() to get a list of nodes,
+    g.get('A') to get a dict of links out of A, and g.get('A', 'B') to get the
+    length of the link from A to B.  'Lengths' can actually be any object at
+    all, and nodes can be any hashable object."""
+
+    def __init__(self, dictionary=None, directed=True):
+        self.dict = dictionary or {}
+        self.directed = directed
+        if not directed:
+            self.make_undirected()
+
+    def make_undirected(self):
+        """Make a digraph into an undirected graph by adding symmetric edges."""
+        for a in list(self.dict.keys()):
+            for (b, dist) in self.dict[a].items():
+                self.connect1(b, a, dist)
+
+    def connect(self, node_a, node_b, distance_val=1):
+        """Add a link from node_a and node_b of given distance_val, and also add the inverse
+        link if the graph is undirected."""
+        self.connect1(node_a, node_b, distance_val)
+        if not self.directed:
+            self.connect1(node_b, node_a, distance_val)
+
+    def connect1(self, node_a, node_b, distance_val):
+        """Add a link from node_a to node_b of given distance_val, in one direction only."""
+        self.dict.setdefault(node_a, {})[node_b] = distance_val
+
+    def get(self, a, b=None):
+        """Return a link distance or a dict of {node: distance} entries.
+        .get(a,b) returns the distance or None;
+        .get(a) returns a dict of {node: distance} entries, possibly {}."""
+        links = self.dict.setdefault(a, {})
+        if b is None:
+            return links
+        else:
+            return links.get(b)
+
+    def nodes(self):
+        """Return a list of nodes in the graph."""
+        return list(self.dict.keys())
+
+
+def UndirectedGraph(dictionary=None):
+    """Build a Graph where every edge (including future ones) goes both ways."""
+    return Graph(dictionary=dictionary, directed=False)
+
+
+def RandomGraph(nodes=list(range(10)), min_links=2, width=400, height=300,
+                curvature=lambda: random.uniform(1.1, 1.5)):
+    """Construct a random graph, with the specified nodes, and random links.
+    The nodes are laid out randomly on a (width x height) rectangle.
+    Then each node is connected to the min_links nearest neighbors.
+    Because inverse links are added, some nodes will have more connections.
+    The distance between nodes is the hypotenuse times curvature(),
+    where curvature() defaults to a random number between 1.1 and 1.5."""
+    g = UndirectedGraph()
+    g.locations = {}
+    # Build the cities
+    for node in nodes:
+        g.locations[node] = (random.randrange(width), random.randrange(height))
+    # Build roads from each city to at least min_links nearest neighbors.
+    for i in range(min_links):
+        for node in nodes:
+            if len(g.get(node)) < min_links:
+                here = g.locations[node]
+
+                def distance_to_node(n):
+                    if n is node or g.get(node, n):
+                        return math.inf
+                    return distance(g.locations[n], here)
+
+                neighbor = nodes.index(min(nodes, key=distance_to_node))
+                d = distance(g.locations[neighbor], here) * curvature()
+                g.connect(node, neighbor, int(d))
+    return g
+
+
+class GraphProblem(Problem):
+    """The problem of searching a graph from one node to another."""
+
+    def __init__(self, initial, goal, graph):
+        super().__init__(initial, goal)
+        self.graph = graph
+
+    def actions(self, state):
+        """The actions at a graph node are just its neighbors."""
+        return list(self.graph.get(state).keys())
+
+    def result(self, state, action):
+        """The result of going to a neighbor is just that neighbor."""
+        return action
+
+    def path_cost(self, c, state1, action, state2):
+        return c + (self.graph.get(state1, state2) or math.inf)
+
+    def h(self, node):
+        """h function is straight-line distance from a node's state to goal."""
+        locs = getattr(self.graph, 'locations', None)
+        if locs:
+            return int(distance(locs[node.state], locs[self.goal]))
+        else:
+            return math.inf
